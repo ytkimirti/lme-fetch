11d4a0e789247c7a39f83f57a6438d1fcd04b6e5
ytkimirti <yusuftaha9@gmail.com>
2021-11-29 23:31:15 +0300

    changes

--- test.py
+++ test.py
@@ -9,60 +9,66 @@ from selenium.webdriver.firefox.options import Options
 from datetime import datetime
 import time
 
-print("Launching driver...")
+#print("Launching driver...")
 
 options = Options()
 options.headless = True
 
 def price_found(price):
-    file = open('data.txt', 'a')
     time_str = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    file.write(f"{time_str} - {price}\n")
+    # file = open('data.txt', 'a')
+    # file.write(f"{time_str} - {price}\n")
+    print(f"{time_str} - {price}")
 
 #This example requires Selenium WebDriver 3.13 or newer
 with webdriver.Firefox(options=options) as driver:
-    print("Loading lme page...")
+    #print("Loading lme page...")
 
     driver.get("https://www.lme.com/")
     
-    print("Page loaded!\n")
+    #print("Page loaded!\n")
 
     while (True):
         #Find prices section
-        print("\tSearching for prices section...")
+        #print("\tSearching for prices section...")
         block_section = driver.find_element(By.CLASS_NAME, "metal-block-container")
     
-        print("\tFound prices section")
+        #print("\tFound prices section")
 
         #Scroll to it
         driver.execute_script("arguments[0].scrollIntoView();", block_section)
         
-        print("\tScrolled to it")
+        #print("\tScrolled to it")
 
         # Wait until that element shows up
         wait = WebDriverWait(driver, 10)
 
-        print("\tWaiting for prices element to load...")
+        #print("\tWaiting for prices element to load...")
         first_result = wait.until(presence_of_element_located((By.CLASS_NAME, "metal-block__price")))
         
-        priceText = first_result.text
+        priceText = ""
+
+        try: 
+            priceText = first_result.text
+        except:
+            continue
 
         if (priceText):
-            print(f"\tPrice text is: {priceText}")
+            #print(f"\tPrice text is: {priceText}")
             
             try:
                 price_float = float(priceText)
-                print(f"PRICE: {priceText}")
+                #print(f"PRICE: {priceText}")
                 price_found(priceText)
                 driver.quit()
                 quit()
             except ValueError:
-                print("\tText cannot be converted to float!")
-            
-        else:
-            print("\tElement is null :(")
+                pass
+                #print("\tText cannot be converted to float!")
+        # else:
+            #print("\tElement is null :(")
         
-        print("\nTrying again...\n")
+        #print("\nTrying again...\n")
         time.sleep(1)
 
   
